f10caff872ad4d95ed7af01e8b5980c8ab37b18c
Richard Boardman <rboardman@rboardman-ltm4.internal.salesforce.com>
2018-09-17 19:30:21 -0700

    username query

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ global.fetch = require('isomorphic-fetch');
 var keys = require('./config/keys');
 var dbx = new Dropbox({ accessToken: keys.dbxAccessToken });
 
-console.log('Started');
+
+dbx.usersGetCurrentAccount()
+  .then(function(response) {
+  	console.log('USER ID');
+    console.log(response);
+  })
+  .catch(function(error) {
+    console.error(error);
+  });
+
+
 dbx.filesListFolder({path: ''})
   .then(function(response) {
+    console.log('TEST DIR LISTING');
     console.log(response);
   })
   .catch(function(error) {
